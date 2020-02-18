4b7dda0890d0e8febe4bdd62ab21a500b3e0815b
JMGOliveira <61151853+JMGOliveira@users.noreply.github.com>
2020-02-18 13:00:44 -0300

    Update Controle_Ingressos.cpp

--- Controle_Ingressos.cpp
+++ Controle_Ingressos.cpp
@@ -1,68 +1,71 @@
 # include <stdio.h>
 # include <stdlib.h>
+# include <iostream>
+
+using namespace std;
 
 char op2 = 0;
 int a = 0, c = 0, fil = 0, assen = 0, f = 20, l = 40;
 int lugares[100][100];
 
 void reservar(){
-    printf("Selecione um assento vago.\n");
+	
+cout << "Selecione um assento vago.\n";
         while (a < f){
             for(c = 0;c < l;c++){
                 if(lugares[c][a] == 0){
-                    printf("Fileira %d assento %d esta livre.\n", a + 1, c + 1);
+                	cout << "Fileira " << a + 1 <<" assento " << c + 1 <<" esta livre.\n";
                 }   
             }
             a++;
         }
-    printf("Selecione a fileira.\n");
-    scanf("%d", &fil);
-    printf("Selecione o assento.\n");
-    scanf("%d", &assen);
+cout << "Selecione a fileira.\n";
+cin >> fil;
+cout << "Selecione o assento\n";
+cin >> assen;
     
     fil = fil - 1;
     assen = assen - 1;
     
     lugares[assen][fil] = 1;
-    printf("O assento %d da fileira %d esta agora reservado.\n", assen + 1, fil + 1);
+cout << "O assento " << assen + 1 << " da fileira " << fil + 1 << " esta agora reservado.\n";
     a = 0;
 }
 
 void devolver(){
-	printf("Selecione um assento reservado.\n");
+	cout << "Selecione um assento reservado.\n";
         while (a < f){
             for(c = 0;c < l;c++){
                 if(lugares[c][a] != 0){
-                    printf("Fileira %d assento %d esta ocupada.\n", a + 1, c + 1);
+                	cout << "Fileira " << a + 1 << " assento " << c + 1 << " esta ocupada.\n";
                 }   
             }
             a++;
         }
-    printf("Selecione a fileira.\n");
-    scanf("%d", &fil);
-    printf("Selecione o assento.\n");
-    scanf("%d", &assen);
+        cout << "Selecione a fileira.\n";
+        cin >> fil;
+        cout << "Selecione o assento.\n";
+        cin >> assen;
     
     fil = fil - 1;
     assen = assen - 1;
     
     lugares[assen][fil] = 0;
-    printf("O assento %d da fileira %d esta agora vago.\n", assen + 1, fil + 1);
+    cout << "O assento " << assen + 1 << " da fleira " << fil + 1 << " esta agora vago.\n";
     a = 0;
 }
 
 int main(){
 
 char op1;
-
-    printf("Este programa tem como base um estabelecimento com 20 fileiras e\n40 assentos em cada uma delas.\nDeseja fazer uma alteracao?\nS-Sim\nN-Nao\n");
+	cout << "Este programa tem como base um estabelecimento com 20 fileiras e\n40 assentos em cada uma delas.\nDeseja fazer uma alteracao?\nS-Sim\nN-Nao\n";  
     op1 = getchar();
     
         if((op1 == 's') || (op1 == 'S')){
-            printf("Informe a quantidade de fileiras.\n");
-            scanf("%d", &f);
-            printf("Informe a quantidade de assentos.\n");
-            scanf("%d", &l);
+        	cout << "Informe a quantidade de fileiras.\n";
+        	cin >> f;
+        	cout << "Informe a quantidade de assentos\n";
+        	cin >> l;
         }
 system("cls");
         while (a < f){
@@ -73,9 +76,9 @@ system("cls");
         }
     a = 0;
     
-    while(op2 != '0'){    
-        printf("Informe a operacao desejada.\n1-Comprar ingresso.\n2-Devolver ingresso.\n3-Trocar de lugar.\n0-Sair.\n");
-        scanf("%s", &op2);
+    while(op2 != '0'){
+		cout <<  "Informe a operacao desejada.\n1-Comprar ingresso.\n2-Devolver ingresso.\n3-Trocar de lugar.\n0-Sair.\n";   
+		cin >> op2;
 system("cls");
             switch(op2){
                 case '1':
@@ -88,7 +91,7 @@ system("cls");
                 
                 case '3':
                 devolver();
-                printf("Selecione agora um assento vago.\n");
+                cout << "Selecione agora um assento vago.\n";
                 reservar();
                 break;
             
